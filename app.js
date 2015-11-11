42bc998bdc30a0fa6fd663156ec567e7ab3050be
lishaojie <lishaojie@smartisan.com>
2015-11-11 19:58:17 +0800

    finish listing file

--- app.js
+++ app.js
@@ -21,9 +21,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/favicon.ico'))
 
 app.use('/', routes);
 app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handlers
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,66 @@
 var express = require('express');
+var Parser = require('parse-listing');
 var router = express.Router();
+var exec = require('child_process').exec;
+var humanize = require('humanize');
 
 router.use(function(req, res, next) {
-  // res.send("requesting: "+req.url);
-  
+    // res.send("requesting: "+req.url);
+    var path = req.url.substr(1);
+    console.log("routing path:" + path);
+    if (path == '') {
+        // list devices attached
+        exec("adb devices", function(error, stdout, stderr) {
+            var devices = stdout.split('\n');
+            if (devices.length < 2) {
+                res.send("no device attached.")
+            };
+            devices.shift();
+            var attached_devices = devices.length;
+            devices = devices.map(function(device_line) {
+                console.log(device_line);
+                return device_line.split('\t').shift();
+            }).filter(function(device) {
+                return !!device;
+            })
+            res.render('index', {
+                title: 'select device',
+                device_list: devices,
+            })
+        })
+    } else {
+        var device_index = path.indexOf('/');
+        var device;
+        var android_path;
+        if (device_index == -1) {
+            device = path;
+            android_path = '/';
+        } else {
+            device = path.substr(0, device_index);
+            android_path = path.substr(device_index);
+        }
+        exec("adb -s " + device + " shell ls -l /" + android_path, function(error, stdout, stderr) {
+            Parser.parseEntries(stdout, function(err, entries) {
+                console.log('got entries: ' + entries.length);
+                entries.forEach(function (entry) {
+                    if (entry.type == 1) {
+                        entry.name += '/';
+                    };
+                    if (entry.size > 0) {
+                        entry.size = humanize.filesize(entry.size);
+                    };
+                })
+                if (path.charAt(path.length - 1) == '/') {
+                    path = path.substr(0, path.length - 1);
+                }
+                path = path.substr(0, path.lastIndexOf('/') + 1);
+                res.render('dir_content', {
+                    files: entries,
+                    upper: '/' + path
+                });
+            });
+        });
+    }
 });
 
 module.exports = router;
